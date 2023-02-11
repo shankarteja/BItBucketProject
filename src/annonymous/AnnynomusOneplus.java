7520c7396fca49441162f882144b751d515cb0d3
shankarteja <121328737+shankarteja@users.noreply.github.com>
2023-02-11 14:52:32 +0530

    AOI_1234
    ADDED ONE  MORE SYS OUT

--- src/annonymous/AnnynomusOneplus.java
+++ src/annonymous/AnnynomusOneplus.java
@@ -9,11 +9,13 @@ public class AnnynomusOneplus {
 			@Override
 			public void process() {
 			System.out.println("hiiiiiiiiii");	
+			System.out.println("hello teja ");
 			}
 			
 			@Override
 			public void method() {
 				System.out.println(" how r u teja ");
+				System.out.println("how r u teja");
 				
 			}
 		};
