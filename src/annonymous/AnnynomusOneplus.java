b3efb78c40eed4c309545f6fe346b409ff30ea90
shankarteja <121328737+shankarteja@users.noreply.github.com>
2023-02-11 15:07:11 +0530

    AOI_123456789
    added new project

--- src/annonymous/AnnynomusOneplus.java
+++ src/annonymous/AnnynomusOneplus.java
@@ -1,24 +1,31 @@
 package annonymous;
 
+import java.security.PublicKey;
+
 public class AnnynomusOneplus {
 
 	public static void main(String[] args) {
 		
 		Oneplus o=new Oneplus() {
 			
+			
 			@Override
 			public void process() {
 			System.out.println("hiiiiiiiiii");	
 			System.out.println("hello teja ");
+			System.out.println("how r u teja");
 			}
 			
 			@Override
 			public void method() {
 				System.out.println(" how r u teja ");
 				System.out.println("how r u teja");
+				System.out.println("prepare well good");
 				
 			}
 		};
+			
+		
 		o.process();
 		o.method();
 	}
